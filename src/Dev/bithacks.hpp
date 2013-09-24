62152415fb967182993cd009a7f1531825453cf6
Matei David <matei.david@oicr.on.ca>
2013-09-24 18:03:25 -0400

    bithacks : currently just bit count

---
+++ src/Dev/bithacks.hpp
@@ -0,0 +1,18 @@
+//-----------------------------------------------
+// Copyright 2013 Ontario Institute for Cancer Research
+// Written by Matei David (mdavid@oicr.on.ca)
+// Released under the GPL license
+//-----------------------------------------------
+
+// Collection of bit hacks
+
+#ifndef __BITHACKS_HPP
+#define __BITHACKS_HPP
+
+#include <stdint.h>
+
+
+int bitcount(uint64_t x);
+
+
+#endif
