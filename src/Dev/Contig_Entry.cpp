89bcf8b61aa3d4da5817eee5807c037cf7b6c7fe
Matei David <matei.david@oicr.on.ca>
2013-10-30 19:55:02 -0400

    Contig_Entry getters for mutations spanning position, and chunks with mutation. Also, cut_mutation ability.

--- src/Dev/Contig_Entry.cpp
+++ src/Dev/Contig_Entry.cpp
@@ -10,9 +10,54 @@
 #include "Read_Entry.hpp"
 #include "print_seq.hpp"
 
+using namespace std;
+
 
 namespace MAC
 {
+    const Mutation* Contig_Entry::cut_mutation(const Mutation* mut_cptr, Size_Type c_offset, Size_Type r_offset)
+    {
+        // find mutation in container
+        Mutation_Cont::iterator it;
+        it = _mut_cont.iterator_to(*mut_cptr);
+        assert(it != _mut_cont.end());
+
+        // cut existing mutation, saving remaining part
+        Mutation m_new;
+        auto mut_modifier = [&] (Mutation& m) { m_new = m.cut(c_offset, r_offset); };
+        bool success;
+        success = _mut_cont.modify(it, mut_modifier);
+        assert(success);
+
+        // insert remaining part
+        Mutation_Cont::iterator it_new;
+        boost::tie(it_new, success) = _mut_cont.insert(m_new);
+
+        return &(*it_new);
+    }
+
+    vector< Read_Chunk_CPtr > Contig_Entry::get_chunks_with_mutation(const Mutation* mut_cptr) const
+    {
+        vector< Read_Chunk_CPtr > res;
+        for (auto it = _chunk_cptr_cont.begin(); it != _chunk_cptr_cont.end(); ++it)
+        {
+            if ((*it)->have_mutation(mut_cptr))
+                res.push_back(*it);
+        }
+        return res;
+    }
+
+    vector< const Mutation* > Contig_Entry::get_mutations_spanning_pos(Size_Type c_pos) const
+    {
+        vector< const Mutation* > res;
+        for (auto it = _mut_cont.begin(); it != _mut_cont.end() and it->get_start() < c_pos; ++it)
+        {
+            if (c_pos < it->get_end())
+                res.push_back(&(*it));
+        }
+        return res;
+    }
+
     std::ostream& operator << (std::ostream& os, const Contig_Entry& rhs)
     {
         os << "(seq=" << rhs.get_seq() << ",\nmut_list=\n  ";
--- src/Dev/Contig_Entry.hpp
+++ src/Dev/Contig_Entry.hpp
@@ -9,6 +9,7 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 #include <memory>
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/sequenced_index.hpp>
@@ -36,12 +37,33 @@ namespace MAC
         /** @name Getters */
         /**@{*/
         const Seq_Type& get_seq() const { return *_seq_ptr; }
+        const Mutation_Cont& get_mutation_cont() const { return _mut_cont; }
         /**@}*/
 
         /** Add to the list of read chunks.
          * @param chunk_cptr Pointer to read chunk object.
          */
         void add_chunk(Read_Chunk_CPtr chunk_cptr) { _chunk_cptr_cont.insert(chunk_cptr); }
+
+        /** Cut mutation at given offsets.
+         * @param mut_cptr Pointer to mutation to cut.
+         * @param c_offset Contig offset inside mutation where to cut.
+         * @param r_offset Read offset inside mutation where to cut.
+         * @return Pointer to mutation object containing leftover part of the original mutation.
+         */
+        const Mutation* cut_mutation(const Mutation* mut_cptr, Size_Type c_offset, Size_Type r_offset);
+
+        /** Get chunks which contain a given mutation.
+         * @param mut_cptr Pointer to mutation.
+         * @return Vector of pointers to chunks that have this mutation.
+         */
+        std::vector< Read_Chunk_CPtr > get_chunks_with_mutation(const Mutation* mut_cptr) const;
+
+        /** Get mutations that completely span a given contig position.
+         * @param c_pos Contig position, 0-based.
+         * @return Vector of pointers to Mutation objects completely spanning c_pos.
+         */
+        std::vector< const Mutation* > get_mutations_spanning_pos(Size_Type c_pos) const;
 
         friend std::ostream& operator << (std::ostream& os, const Contig_Entry& rhs);
 
