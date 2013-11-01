9047f8c2bf39501b3b498cbbb080b0365ed8164b
Matei David <matei.david@oicr.on.ca>
2013-11-01 18:36:21 -0400

    modifiers for boost containers

--- src/Dev/MAC_forward.hpp
+++ src/Dev/MAC_forward.hpp
@@ -8,6 +8,8 @@
 #define __MAC_FORWARD_HPP
 
 #include <string>
+#include <functional>
+#include <cassert>
 
 
 /** Multi-Allelic Contig namespace. */
@@ -24,6 +26,43 @@ namespace MAC
     class Read_Chunk;
     class Read_Entry;
     class Contig_Entry;
+}
+
+
+/** General-purpose modifier for boost containers.
+ * @param cont A container.
+ * @param e_it Iterator to element inside container.
+ * @param modifier Modifier to apply to element.
+ */
+template <class Container>
+inline void modify_element(Container& cont, typename Container::iterator e_it, std::function<void(typename Container::value_type&)> modifier)
+{
+    assert(e_it != cont.end());
+    bool success = cont.modify(e_it, modifier);
+    assert(success);
+}
+
+/** General-purpose modifier for boost containers.
+ * @param cont A container.
+ * @param e Constant reference to element inside container.
+ * @param modifier Modifier to apply to element.
+ */
+template <class Container>
+inline void modify_element(Container& cont, const typename Container::value_type& e, std::function<void(typename Container::value_type&)> modifier)
+{
+    modify_element<Container>(cont, cont.iterator_to(e), modifier);
+}
+
+/** General-purpose modifier for boost containers.
+ * @param cont A container.
+ * @param e_cptr Constant pointer to element inside container.
+ * @param modifier Modifier to apply to element.
+ */
+template <class Container>
+inline void modify_element(Container& cont, const typename Container::value_type* e_cptr, std::function<void(typename Container::value_type&)> modifier)
+{
+    assert(e_cptr != NULL);
+    modify_element<Container>(cont, cont.iterator_to(*e_cptr), modifier);
 }
 
 
