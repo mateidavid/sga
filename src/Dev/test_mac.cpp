a2443b46dd1cbbc16b764c481cc048298a0bc8f2
Matei David <matei.david@oicr.on.ca>
2013-11-05 12:39:52 -0500

    test read breaking with no mutations

--- src/Dev/test_mac.cpp
+++ src/Dev/test_mac.cpp
@@ -13,8 +13,12 @@ int main()
 {
     Graph g;
 
-    g.add_read(new string("001"), new Seq_Type("ACGT"));
-    cerr << "g after add_read:" << indent::inc << indent::nl << g << indent::dec << indent::nl;
+    g.add_read(new string("001"), new Seq_Type("ACGTGGGCATTTGACATAGCTGGATAAACCCTCAGATCGGAGTTACTTAG"));
+    cerr << "g after adding read 001:" << indent::inc << indent::nl << g << indent::dec << indent::nl;
+    g.add_read(new string("002"), new Seq_Type("AAACCCTCAGATCGGAGTTACTTAGGTCAGAGTTTAGCTTGATCCTTTAG"));
+    cerr << "g after adding read 002:" << indent::inc << indent::nl << g << indent::dec << indent::nl;
+    g.add_overlap(string("001"), string("002"), 25, 25, 0, 25, 0, string("25M"));
+    cerr << "g after adding overlap 001-002:" << indent::inc << indent::nl << g << indent::dec << indent::nl;
 
     return EXIT_SUCCESS;
 }
