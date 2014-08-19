8b791787ba427fbcfd1d3e336b083cf3702cceff
Matei David <matei@cs.toronto.edu>
2014-08-19 17:38:16 -0400

    add ptree prefix for logging

--- src/Dev/ptree.hpp
+++ src/Dev/ptree.hpp
@@ -19,17 +19,31 @@ class ptree
     : public boost::property_tree::ptree
 {
 public:
+    ptree(const std::string& root = std::string(), const boost::property_tree::ptree& t = boost::property_tree::ptree())
+      : prefix_(root.empty()? root : root + ".")
+    {
+        if (not root.empty())
+        {
+            put_child(root, t);
+        }
+        else
+        {
+            *static_cast< boost::property_tree::ptree* >(this) = t;
+        }
+    }
+
     template < typename T >
     ptree& put(std::string key, const T& val)
     {
-        boost::property_tree::ptree::put(key, val);
+        boost::property_tree::ptree::put(prefix_ + key, val);
         return *this;
     }
     ptree& put(std::string key, const boost::property_tree::ptree& pt)
     {
-        boost::property_tree::ptree::put_child(key, pt);
+        boost::property_tree::ptree::put_child(prefix_ + key, pt);
         return *this;
     }
+    std::string prefix_;
 };
 
 
