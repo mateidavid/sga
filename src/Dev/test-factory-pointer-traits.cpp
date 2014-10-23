c520f386a56d1dc2af3b37e4929c18d2fbff00be
Matei David <matei@cs.toronto.edu>
2014-10-23 13:04:36 -0400

    remove dead code

--- src/Dev/test-factory-pointer-traits.cpp
+++ src/Dev/test-factory-pointer-traits.cpp
@@ -25,18 +25,14 @@ static_assert(is_same< bounded::Pointer< const_A >, bounded::Pointer< const cons
 
 static_assert(is_same< typename a_ptr_tr::element_type, A >::value, "a_ptr_tr::element_type != A");
 static_assert(is_same< typename a_ptr_tr::pointer, a_ptr_t >::value, "a_ptr_tr::pointer != a_ptr_t");
-//static_assert(is_same< typename a_ptr_tr::const_pointer, a_const_ptr_t >::value, "a_ptr_tr::const_pointer != a_const_ptr_t");
 static_assert(is_same< typename a_ptr_tr::reference, a_ref_t >::value, "a_ptr_tr::reference != a_ref_t");
 
 static_assert(is_same< boost::intrusive::detail::type_rebinder< bounded::Pointer< A >, const A >::type, bounded::Pointer< const A > >::value, "rebind error");
 
 static_assert(is_same< typename a_ptr_tr::template rebind_pointer< A >::type, a_ptr_t >::value, "a_ptr_tr::rebind_pointer< A > != a_ptr_t");
-//static_assert(is_same< typename a_ptr_tr::template rebind_pointer< void >::type, void* >::value, "a_ptr_tr::rebind_pointer< void > != void*");
-//static_assert(is_same< typename a_ptr_tr::template rebind_pointer< B >::type, B* >::value, "a_ptr_tr::rebind_pointer< B > != B*");
 static_assert(is_same< typename a_ptr_tr::template rebind_pointer< const A >::type, a_const_ptr_t >::value, "a_ptr_tr::rebind_pointer< const A > != a_const_ptr_t");
 
 static_assert(is_same< typename a_const_ptr_tr::pointer, a_const_ptr_t >::value, "a_const_ptr_tr::pointer != a_const_ptr_t");
-//static_assert(is_same< typename a_const_ptr_tr::const_pointer, a_const_ptr_t >::value, "a_const_ptr_tr::const_pointer != a_const_ptr_t");
 static_assert(is_same< typename a_const_ptr_tr::template rebind_pointer< A >::type, a_ptr_t >::value, "a_const_ptr_tr::rebind_pointer< A > != a_ptr_t");
 
 int main()
