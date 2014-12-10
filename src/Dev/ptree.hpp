4c2297a65f89edf45e0324ea03ef1c9ccb8d705f
Matei David <matei@cs.toronto.edu>
2014-12-10 13:32:42 -0500

    use to_ptree memfn if it exists, fall back to ptree ctor

--- src/Dev/ptree.hpp
+++ src/Dev/ptree.hpp
@@ -13,6 +13,7 @@
 #include <functional>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/tti/has_member_function.hpp>
 
 
 /** Fix return value from put() and put_child() to allow chaining. */
@@ -55,6 +56,38 @@ std::ostream& operator << (std::ostream& os, const boost::property_tree::ptree& 
     return os;
 }
 
+namespace detail
+{
+
+// if T has member function to_ptree, call that
+template < typename T, bool has_to_ptree >
+struct elem_to_ptree_impl
+{
+    boost::property_tree::ptree operator() (const T& e)
+    {
+        return e.to_ptree();
+    }
+};
+
+// if T doesn't have member function to_ptree, try to construct ptree from T
+template < typename T >
+struct elem_to_ptree_impl< T, false >
+{
+    boost::property_tree::ptree operator() (const T& e)
+    {
+        return boost::property_tree::ptree(e);
+    }
+};
+
+BOOST_TTI_HAS_MEMBER_FUNCTION(to_ptree)
+
+// functor that converts an object of type T to boost::property_tree::ptree
+template < typename T >
+struct elem_to_ptree : elem_to_ptree_impl< T, has_member_function_to_ptree< const T, boost::property_tree::ptree >::value >
+{};
+
+} // namespace detail
+
 template < typename Cont, typename Elem_Type = typename Cont::value_type >
 boost::property_tree::ptree cont_to_ptree(const Cont& cont, std::function<boost::property_tree::ptree(const Elem_Type&)> to_ptree)
 {
@@ -66,11 +99,12 @@ boost::property_tree::ptree cont_to_ptree(const Cont& cont, std::function<boost:
     }
     return pt;
 }
+
 template < typename Cont >
 boost::property_tree::ptree cont_to_ptree(const Cont& cont)
 {
-    typedef typename Cont::iterator::value_type elem_type;
-    return cont_to_ptree< Cont, elem_type >(cont, [] (const elem_type& e) { return e.to_ptree(); });
+    typedef typename Cont::const_iterator::value_type elem_type;
+    return cont_to_ptree< Cont, elem_type >(cont, detail::elem_to_ptree< elem_type >());
 }
 
 #endif
