eaeb698730574ba3a3906368388e00c57dfa62dd
Matei David <matei@cs.toronto.edu>
2015-10-22 13:58:56 -0400

    fix: return by value from identity functor to allow default values

--- src/Util/algorithm_extra.hpp
+++ src/Util/algorithm_extra.hpp
@@ -8,11 +8,13 @@ namespace detail
 {
 
 struct Identity {
+    //template < typename U >
+    //constexpr auto operator () (U&& v) const noexcept -> decltype(std::forward< U >(v))
+    //{
+    //    return std::forward< U >(v);
+    //}
     template < typename U >
-    constexpr auto operator () (U&& v) const noexcept -> decltype(std::forward< U >(v))
-    {
-        return std::forward< U >(v);
-    }
+    U operator () (const U& v) const { return v; }
 }; // struct Identity
 
 } //namespace detail
